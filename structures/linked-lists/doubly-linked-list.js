cf87261f4cf014d820fb386e5bff646bdefa981d
tomdaniels <thomsondaniels@hotmail.com>
2022-01-19 12:18:28 +1100

    push onto a dll

--- structures/linked-lists/doubly-linked-list.js
+++ structures/linked-lists/doubly-linked-list.js
@@ -12,6 +12,24 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
+      this.length++;
+    }
+    return this;
+  }
 }
 
-// const dll = new DoublyLinkedList()
+const dll = new DoublyLinkedList()
+dll.push(99);
+dll.push(100);
+
+console.log(dll);
