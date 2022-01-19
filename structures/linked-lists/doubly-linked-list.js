6cf1709485dfcba484de879b0dffa55f65c8b7c0
tomdaniels <thomsondaniels@hotmail.com>
2022-01-19 11:41:23 +1100

    init doubly linked list

---
+++ structures/linked-lists/doubly-linked-list.js
@@ -0,0 +1,17 @@
+class Node {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+// const dll = new DoublyLinkedList()
