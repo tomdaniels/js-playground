f6d62a5e3905637dce7483fb7d1f4a228283b915
tomdaniels <thomsondaniels@hotmail.com>
2022-01-26 15:11:50 +1100

    push onto stack

--- structures/stacks.js
+++ structures/stacks.js
@@ -11,4 +11,21 @@ class Stack {
     this.last = null;
     this.size = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = this.first;
+    } else {
+      newNode.next = this.first;
+      this.first = newNode;
+    }
+    this.size++;
+    return this;
+  }
 }
+
+const stack = new Stack();
+
+stack.push("first");
