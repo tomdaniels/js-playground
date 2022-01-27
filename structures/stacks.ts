78617077f8bd78080d5aaa91444b5fe1fdfd29ad
tomdaniels <thomsondaniels@hotmail.com>
2022-01-27 12:01:58 +1100

    convert stack to typescript

--- structures/stacks.ts
+++ structures/stacks.ts
@@ -1,6 +1,9 @@
-class Node {
-  constructor(value) {
-    this.value = value;
+class StackNode {
+  value: any;
+  next: StackNode | null;
+
+  constructor(val: any) {
+    this.value = val;
     this.next = null;
   }
 }
@@ -8,14 +11,18 @@ class Node {
 // stack works as a singly linked list, `shift` and `unshift` items into the stack
 // giving constant time for insert and delete & follow LIFO
 class Stack {
+  first: StackNode | null;
+  last: StackNode | null;
+  size: number;
+
   constructor() {
     this.first = null;
     this.last = null;
     this.size = 0;
   }
 
-  push(val) {
-    const newNode = new Node(val);
+  push(val: any): any {
+    const newNode = new StackNode(val);
     if (!this.first) {
       this.first = newNode;
       this.last = this.first;
@@ -27,14 +34,14 @@ class Stack {
     return this;
   }
 
-  pop() {
+  pop(): any {
     if (!this.first) return undefined;
   
       let current = this.first;
       this.first = current.next;
-      this.length--;
+      this.size--;
   
-      if (this.length === 0) {
+      if (this.size === 0) {
         this.last = null;
       }
   
@@ -45,3 +52,6 @@ class Stack {
 const stack = new Stack();
 
 stack.push("first");
+stack.push("second");
+stack.push("third");
+
