9743a577b043350716d50afc4b3749e14a6c594d
Tom Daniels <tom.daniels@192-168-1-111.tpgi.com.au>
2022-02-02 16:21:00 +1100

    bts skeleton

---
+++ structures/binary-search-tree.ts
@@ -0,0 +1,18 @@
+class TreeNode {
+  value: any;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(value: any) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  root: TreeNode | null;
+  constructor() {
+    this.root = null;
+  }
+}
